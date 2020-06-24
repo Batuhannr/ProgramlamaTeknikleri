d9d6c3460c45820c6cfcd75bf0d0fd4cde0815e7
Batuhannr <batuhnnr@gmail.com>
2020-06-24 13:45:32 +0300

    s

---
+++ 20191132044.js
@@ -0,0 +1,129 @@
+var key = 10063474;
+var ek = new Date();
+var ek1 = ek.getTime();
+keyT= prompt("Lutfen Sifre Giriniz")
+ek1m = ek1.toString();
+
+if (key == keyT) {
+    islem = prompt("Sifrelemek icin 1 Desifrelemek icin 2 giriniz!")
+    if (islem == 1 ) {
+        text = prompt("Metni girin: ");
+        var uzun = text.length;
+        if (uzun == 1 ) {
+            text1 = text[0] + ek1m[0];
+            document.write(text1);
+        }
+        if (uzun == 2 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1];
+            document.write(text1);
+        }
+        if (uzun == 3 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2];
+            document.write(text1);
+        }
+        if (uzun == 4 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3];
+            document.write(text1);
+        }
+        if (uzun == 5 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4];
+            document.write(text1);
+        }
+        if (uzun == 6 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5];
+            document.write(text1);
+        }
+        if (uzun == 7 ) {
+            text1 = text[0] + ek1m[0]  + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6];
+            document.write(text1);
+        }
+        if (uzun == 8 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7];
+            document.write(text1);
+        }
+        if (uzun == 9 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8];
+            document.write(text1);
+        }
+        if (uzun == 10 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9];
+            document.write(text1);
+        }
+        if (uzun == 11 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9] + text[10]+ ek1m[10];
+            document.write(text1);
+        }
+        if (uzun == 12 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9]+ text[10]+ ek1m[10]+ text[11]+ ek1m[11];
+            document.write(text1);
+        }
+        if (uzun == 13 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9]+ text[10]+ ek1m[10]+ text[11]+ ek1m[11]+ text[12]+ ek1m[12];
+            document.write(text1);
+        }
+    
+        if (uzun == 14 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9]+ text[10]+ ek1m[10]+ text[11]+ ek1m[11]+ text[12]+ ek1m[12]+ text[13]+ ek1m[13];
+            document.write(text1);
+        }
+        if (uzun == 15 ) {
+            text1 = text[0] + ek1m[0] + text[1] + ek1m[1] + text[2] + ek1m[2] + text[3] + ek1m[3] + text[4] + ek1m[4] + text[5] + ek1m[5]+ text[6] + ek1m[6] + text[7] + ek1m[7] + text[8] + ek1m[8] + text[9] + ek1m[9]+ text[10]+ ek1m[10]+ text[11]+ ek1m[11]+ text[12]+ ek1m[12]+ text[13]+ ek1m[13]+ text[14]+ ek1m[14];
+            document.write(text1);
+        }
+    
+    }
+    else if (islem == 2 ) {
+        text = prompt("Metni girin: ");
+        var uzun = text.length;
+        
+        text1=text.replace(1,"");
+        text2=text1.replace(2,"");
+        text3=text2.replace(3,"");
+        text4=text3.replace(4,"");
+        text5=text4.replace(5,"");
+        text6=text5.replace(6,"");
+        text7=text6.replace(7,"");
+        text8=text7.replace(8,"");
+        text9=text8.replace(9,"");
+        text10=text9.replace(0,"");
+        text11=text10.replace(1,"");
+        text12=text11.replace(2,"");
+        text13=text12.replace(3,"");
+        text14=text13.replace(4,"");
+        text15=text14.replace(5,"");
+        text16=text15.replace(6,"");
+        text17=text16.replace(7,"");
+        text18=text17.replace(8,"");
+        text19=text18.replace(9,"");
+        text20=text19.replace(0,"");
+        text21=text20.replace(1,"");
+        text22=text21.replace(2,"");
+        text23=text22.replace(3,"");
+        text24=text23.replace(4,"");
+        text25=text24.replace(5,"");
+        text26=text25.replace(6,"");
+        text27=text26.replace(7,"");
+        text28=text27.replace(8,"");
+        text29=text28.replace(9,"");
+        text30=text29.replace(0,"");
+        text31=text30.replace(1,"");
+        text32=text31.replace(2,"");
+        text33=text32.replace(3,"");
+        text34=text33.replace(4,"");
+        text35=text34.replace(5,"");
+        text36=text35.replace(6,"");
+        text37=text36.replace(7,"");
+        text38=text37.replace(8,"");
+        text39=text38.replace(9,"");
+        text40=text39.replace(0,"");
+        document.write(text40);    
+    
+    }
+    else {
+        alert("Gecersiz Islem Girdiniz")
+    }
+}
+else {
+    alert("Gecersiz Sifre")
+}
+
